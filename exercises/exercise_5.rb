08f9720d84074df601f5a4ff96153dcc8e4bc20c
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-01 21:17:16 -0400

    Calculate sum, avg and number of stores

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total = Store.sum(:annual_revenue)
+@average = Store.average(:annual_revenue)
+@rich_stores=Store.where("annual_revenue > ?" , 1000000)
+number_of_rich_store = @rich_stores.count
+
+puts "total sum of annual revenue of all stores -> #{@total}"
+puts "average annual revenue of all stores -> #{@average}"
+puts "number of rich store making more than 1M is #{number_of_rich_store}"
